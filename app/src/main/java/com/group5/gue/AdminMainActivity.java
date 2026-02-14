3f1db1fae3831cc1156e3e80809b9ba55885834b
Levente Magyar <l.magyar@student.tue.nl>
2026-02-14 20:40:09 +0100

    Connected with supabase and authentication set up

---
+++ app/src/main/java/com/group5/gue/AdminMainActivity.java
@@ -0,0 +1,30 @@
+package com.group5.gue;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.group5.gue.databinding.ActivityAdminMainBinding;
+
+public class AdminMainActivity extends AppCompatActivity {
+
+    private ActivityAdminMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityAdminMainBinding.inflate(getLayoutInflater());
+
+        EdgeToEdge.enable(this);
+        setContentView(binding.getRoot());
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.admin_main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
