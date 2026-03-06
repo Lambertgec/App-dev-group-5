9aa87c7fb86b05d49bc1f8ffc9406e7510c6aa66
Austeja <a.bolyte@student.tue.nl>
2026-03-06 11:49:21 +0100

    Removed unused code, and unnecessary TODO's

--- app/src/main/java/com/group5/gue/MapFragment.java
+++ app/src/main/java/com/group5/gue/MapFragment.java
@@ -30,9 +30,7 @@ import android.widget.Button;
 
 
 /**
- * A simple {@link Fragment} subclass.
- * Use the {@link MapFragment#newInstance} factory method to
- * create an instance of this fragment.
+ * A simple {@link Fragment} subclass for displaying the TU/e map with markers.
  */
 public class MapFragment extends Fragment implements OnMapReadyCallback {
     private GoogleMap mMap;
@@ -40,44 +38,14 @@ public class MapFragment extends Fragment implements OnMapReadyCallback {
     private static final int LOCATION_PERMISSION_REQUEST = 1;
     LatLng tueCampus = new LatLng(51.448, 5.489);
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
     public MapFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment MapFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static MapFragment newInstance(String param1, String param2) {
-        MapFragment fragment = new MapFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
@@ -137,7 +105,7 @@ public class MapFragment extends Fragment implements OnMapReadyCallback {
         mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(tueCampus, 16f));
 
         // Adding markers to the Map
-        // TODO: get the exact coordinates from database + additional info
+        // TODO: get the exact coordinates from database + additional info + add more buildings
         LatLng metaforum = new LatLng(51.447868, 5.487455);
         LatLng atlas = new LatLng(51.44784, 5.48605);
         LatLng auditorium = new LatLng(51.447910, 5.484945);
@@ -159,7 +127,6 @@ public class MapFragment extends Fragment implements OnMapReadyCallback {
                 .icon(BitmapDescriptorFactory.defaultMarker(
                         BitmapDescriptorFactory.HUE_BLUE))
                 .snippet("Contains most lecture rooms"));
-        // TODO: add more buildings
     }
     @Override
     public void onRequestPermissionsResult(int requestCode,
