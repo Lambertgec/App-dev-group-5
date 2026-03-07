2767544e4b12ff456a1f867892c0295610072e7b
Ela-6 <e.dogrusoz@student.tue.nl>
2026-03-07 01:41:36 +0100

    collectibles: added description and blackout

--- app/src/main/java/com/group5/gue/CollectFragment.java
+++ app/src/main/java/com/group5/gue/CollectFragment.java
@@ -15,6 +15,8 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.PopupMenu;
+import android.widget.TextView;
+import android.widget.Toast;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -55,7 +57,13 @@ public class CollectFragment extends Fragment {
     }
 
     Button col1;
+    TextView bocol1;
     Button col2;
+    Button xBut;
+
+    boolean userOwnCollect1 = true; //should instead get data from database
+    TextView text1;
+    TextView text2;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -67,24 +75,44 @@ public class CollectFragment extends Fragment {
 
     }
 
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View rv = inflater.inflate(R.layout.fragment_collect, container, false);
-        col1 = rv.findViewById(R.id.col1);
-        col1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                PopupMenu popupMenu1 = new PopupMenu(getActivity().getApplicationContext(), v);
-            }
-        });
-        return  rv;    }
 
+        return  rv;    }
+    public void setBlackout(View view) {
+        bocol1 = view.findViewById(R.id.bocol1);
+        if (userOwnCollect1){
+            bocol1.setVisibility(view.INVISIBLE);
+        }
+    }
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
+        setBlackout(view);
+        text1 = view.findViewById(R.id.text1);
+        text1.setVisibility(view.INVISIBLE);
+        text2 = view.findViewById(R.id.text2);
+        text2.setVisibility(view.INVISIBLE);
+
+        xBut = view.findViewById(R.id.xbut);
+        xBut.setVisibility(view.INVISIBLE);
+
+        xBut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                text1.setVisibility(view.INVISIBLE);
+                text2.setVisibility(view.INVISIBLE);
+                xBut.setVisibility(view.INVISIBLE);
+            }
+        });
+
         col1 = view.findViewById(R.id.col1);
+
         col1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -98,7 +126,15 @@ public class CollectFragment extends Fragment {
                             startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.youtube.com/watch?v=xvFZjo5PgG0")));
                             return true;
                         } else if (item.getItemId() == R.id.desc){
-                            startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.youtube.com/watch?v=fC7oUOUEEi4")));
+                            //startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.youtube.com/watch?v=fC7oUOUEEi4")));
+                            if (userOwnCollect1){
+                                text1.setVisibility(view.VISIBLE);
+                                xBut.setVisibility(view.VISIBLE);
+                            } else {
+                                Toast toast = Toast.makeText(getActivity().getApplicationContext(), "not owned", Toast.LENGTH_SHORT);
+                                toast.show();
+                            }
+
                             return true;
                         }
                         return false;
@@ -108,28 +144,7 @@ public class CollectFragment extends Fragment {
         });
 
         //this part can be added for every collectible, not sure if theres a better way than to copy paste code for all collectibles.
-        col2 = view.findViewById(R.id.col2);
-        col2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                PopupMenu popupMenu1 = new PopupMenu(getActivity().getApplicationContext(), v);
-                popupMenu1.inflate(R.menu.popup1);
-                popupMenu1.show();
-                popupMenu1.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
-                    @Override
-                    public boolean onMenuItemClick(MenuItem item) {
-                        if (item.getItemId() == R.id.modify) {
-                            startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.youtube.com/watch?v=xvFZjo5PgG0")));
-                            return true;
-                        } else if (item.getItemId() == R.id.desc){
-                            startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.youtube.com/watch?v=fC7oUOUEEi4")));
-                            return true;
-                        }
-                        return false;
-                    }
-                });
-            }
-        });
+
 
     }
 }
