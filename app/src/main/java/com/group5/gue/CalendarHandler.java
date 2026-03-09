eeb74a731db6af3b5a638951d08ea505db25a5e2
Maria Dragan <m.m.dragan@student.tue.nl>
2026-03-09 12:08:41 +0100

    calendar: add getStartingSoon

--- app/src/main/java/com/group5/gue/CalendarHandler.java
+++ app/src/main/java/com/group5/gue/CalendarHandler.java
@@ -78,6 +78,22 @@ public class CalendarHandler {
         return submitQuery(selection, selectionArgs);
     }
 
+    public ArrayList<Event> getStartingSoon() {
+        String selection =
+                CalendarContract.Calendars.CALENDAR_DISPLAY_NAME + " = ? AND " +
+                        CalendarContract.Events.DTSTART + " >= ? AND " +
+                        CalendarContract.Events.DTSTART + " <= ?";
+
+        Long timeNow = System.currentTimeMillis();
+
+        String[] selectionArgs = new String[]{
+                this.calendarName,
+                String.valueOf(timeNow),
+                String.valueOf(timeNow + 3600000)};
+
+        return submitQuery(selection, selectionArgs);
+    }
+
 
     public ArrayList<Event> getDay(Long startOfDay) {
         String selection =
