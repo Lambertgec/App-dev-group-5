cd9f0e05d8b16d35fae42cabbd558ead3fb152f9
Austeja <a.bolyte@student.tue.nl>
2026-03-11 13:52:06 +0100

    Map: the database is now used to fetch building and room information to display them on the map. Buttons for switching between floors has also been added

--- app/src/main/java/com/group5/gue/MapFragment.java
+++ app/src/main/java/com/group5/gue/MapFragment.java
@@ -19,6 +19,8 @@ import com.google.android.gms.maps.model.MapStyleOptions;
 import android.content.res.Resources;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.group5.gue.data.annotation.AnnotationRepository;
+import com.group5.gue.data.model.Annotation;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
@@ -27,57 +29,35 @@ import androidx.core.content.ContextCompat;
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
 import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.List;
 
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link MapFragment#newInstance} factory method to
- * create an instance of this fragment.
  */
 public class MapFragment extends Fragment implements OnMapReadyCallback {
     private GoogleMap mMap;
     private FusedLocationProviderClient fusedLocationClient;
     private static final int LOCATION_PERMISSION_REQUEST = 1;
     LatLng tueCampus = new LatLng(51.448, 5.489);
+    AnnotationRepository repository = AnnotationRepository.Companion.getInstance();
+    private List<Annotation> annotationList;
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    private int currentFloor = -2; // starting level
+    private Button btnFloorUp, btnFloorDown;
+    private TextView tvFloorLevel;
+    private boolean mapReady = false;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
     public MapFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment MapFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static MapFragment newInstance(String param1, String param2) {
-        MapFragment fragment = new MapFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
@@ -93,6 +73,9 @@ public class MapFragment extends Fragment implements OnMapReadyCallback {
 
         fusedLocationClient = LocationServices.getFusedLocationProviderClient(requireActivity());
         Button btnCenterTue = view.findViewById(R.id.btn_center_tue);
+        btnFloorUp = view.findViewById(R.id.btn_floor_up);
+        btnFloorDown = view.findViewById(R.id.btn_floor_down);
+        tvFloorLevel = view.findViewById(R.id.tv_floor_level);
 
         SupportMapFragment mapFragment =
                 (SupportMapFragment) getChildFragmentManager()
@@ -101,66 +84,143 @@ public class MapFragment extends Fragment implements OnMapReadyCallback {
         if (mapFragment != null) {
             mapFragment.getMapAsync(this);
         }
+
+        // Fetches building data from the database
+        fetchAnnotations();
+
+        updateFloorLevel();
+
+        // Centers on TU/e when button is clicked
         btnCenterTue.setOnClickListener(v -> {
             if (mMap != null) {
                 mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(tueCampus, 16f));
             }
         });
 
+        // Buttons to change floor
+        btnFloorUp.setOnClickListener(v -> {
+            if (currentFloor < 8) {
+                currentFloor++;
+                updateFloorLevel();
+            }
+        });
+
+        btnFloorDown.setOnClickListener(v -> {
+            if (currentFloor > -2) {
+                currentFloor--;
+                updateFloorLevel();
+            }
+        });
+    }
+
+    /**
+     * Updates the building markers displayed depending on the current floor level
+     */
+    private void updateFloorLevel() {
+        if (currentFloor == -2) {
+            tvFloorLevel.setText("Buildings");
+        } else tvFloorLevel.setText("Level " + currentFloor);
+
+        // Update markers for this floor
+        showMarkersForFloor(currentFloor);
+
+        // Enable/disable buttons depending on floor
+        btnFloorUp.setEnabled(currentFloor < 8);
+        btnFloorDown.setEnabled(currentFloor > -2);
+
+        // Gray out disabled buttons
+        btnFloorUp.setAlpha(currentFloor < 8 ? 1f : 0.5f);
+        btnFloorDown.setAlpha(currentFloor > -2 ? 1f : 0.5f);
+    }
+
+    private void showMarkersForFloor(int floor) {
+        if (mMap == null || annotationList == null) return;
+
+        mMap.clear(); // remove previous markers
+
+        for (Annotation annotation : annotationList) {
+            String levelStr = annotation.getLevel();
+
+            // Case 1: buildings (no level)
+            if ((levelStr == null) && floor == -2) {
+                LatLng pos = new LatLng(annotation.getLatitude(), annotation.getLongitude());
+                mMap.addMarker(new MarkerOptions()
+                        .position(pos)
+                        .title(annotation.getBuilding())
+                        .snippet(annotation.getRoomName())
+                        .icon(BitmapDescriptorFactory.defaultMarker(
+                                BitmapDescriptorFactory.HUE_BLUE
+                        )));
+                continue;
+            }
+
+            // Case 2: rooms (level is numeric)
+            if (levelStr != null) {
+                try {
+                    int annotationLevel = Integer.parseInt(levelStr);
+                    if (annotationLevel == floor && floor > -2) {
+                        LatLng pos
+                                = new LatLng(annotation.getLatitude(), annotation.getLongitude());
+                        mMap.addMarker(new MarkerOptions()
+                                .position(pos)
+                                .title(annotation.getBuilding())
+                                .snippet(annotation.getRoomName())
+                                .icon(BitmapDescriptorFactory.defaultMarker(
+                                        BitmapDescriptorFactory.HUE_BLUE
+                                )));
+                    }
+                } catch (NumberFormatException e) {
+                    // Skip rows with invalid level data
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    /**
+     *
+     */
+    private void fetchAnnotations() {
+        repository.getAll(new kotlin.jvm.functions.Function1<List<Annotation>, kotlin.Unit>() {
+            @Override
+            public kotlin.Unit invoke(List<Annotation> annotations) {
+                annotationList = annotations;
+
+                // Only update markers if map is ready
+                if (mapReady) {
+                    updateFloorLevel();
+                }
+
+                return kotlin.Unit.INSTANCE;
+            }
+        });
     }
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+        mapReady = true;
 
         mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
         try {
-            mMap.setMapStyle(MapStyleOptions.loadRawResourceStyle(
-                    getContext(), R.raw.map_style));
+            mMap.setMapStyle(MapStyleOptions.loadRawResourceStyle(getContext(), R.raw.map_style));
         } catch (Resources.NotFoundException e) {
             e.printStackTrace();
         }
 
-        if (ContextCompat.checkSelfPermission(requireContext(),
-                Manifest.permission.ACCESS_FINE_LOCATION)
+        if (ContextCompat.checkSelfPermission(requireContext(), Manifest.permission.ACCESS_FINE_LOCATION)
                 == PackageManager.PERMISSION_GRANTED) {
-
             mMap.setMyLocationEnabled(true);
-
-        } else {
-            requestPermissions(
-                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
-                    LOCATION_PERMISSION_REQUEST
-            );
         }
 
         mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(tueCampus, 16f));
 
-        // Adding markers to the Map
-        // TODO: get the exact coordinates from database + additional info
-        LatLng metaforum = new LatLng(51.447868, 5.487455);
-        LatLng atlas = new LatLng(51.44784, 5.48605);
-        LatLng auditorium = new LatLng(51.447910, 5.484945);
-
-        mMap.addMarker(new MarkerOptions()
-                .position(metaforum)
-                .title("MF (Metaforum)")
-                .icon(BitmapDescriptorFactory.defaultMarker(
-                        BitmapDescriptorFactory.HUE_BLUE))
-                .snippet("Contains the TU/e library and ESA desk"));
-        mMap.addMarker(new MarkerOptions()
-                .position(atlas)
-                .title("Atlas")
-                .icon(BitmapDescriptorFactory.defaultMarker(
-                        BitmapDescriptorFactory.HUE_BLUE)));
-        mMap.addMarker(new MarkerOptions()
-                .position(auditorium)
-                .title("Aud (Auditorium)")
-                .icon(BitmapDescriptorFactory.defaultMarker(
-                        BitmapDescriptorFactory.HUE_BLUE))
-                .snippet("Contains most lecture rooms"));
-        // TODO: add more buildings
+        // Add markers if annotations are already loaded
+        if (annotationList != null && !annotationList.isEmpty()) {
+            updateFloorLevel();
+        }
     }
+
     @Override
     public void onRequestPermissionsResult(int requestCode,
                                            @NonNull String[] permissions,
